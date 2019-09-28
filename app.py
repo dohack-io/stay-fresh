c3b08c041ded22424d9c16d6226ec5729b822b87
rijun <richard.jung001@stud.fh-dortmund.de>
2019-09-28 20:44:01 +0200

    Added send-data endpoind

--- app.py
+++ app.py
@@ -1,9 +1,54 @@
-from flask import Flask, request, send_from_directory
-
+import random
+from datetime import datetime
+from flask import Flask, request, send_from_directory, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/')
 def root():
     return send_from_directory('', 'index.html')
+
+
+@app.route('/get-data')
+def get_data():
+    return_data = {
+        'extern': {
+            'id': "STAT1",
+            'name': "Station 1",
+            'date_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'no2': random.randint(1, 1000),
+            'pm10': random.randint(1, 1000)
+        },
+        'intern': [
+            {
+                'id': "R1",
+                'name': "Raum 1",
+                'temp': random.randint(1, 1000),
+                'hum': random.randint(1, 1000),
+                'co2': random.randint(1, 1000),
+                'voc': random.randint(1, 1000)
+            },
+            {
+                'id': "R2",
+                'name': "Raum 2",
+                'temp': random.randint(1, 1000),
+                'hum': random.randint(1, 1000),
+                'co2': random.randint(1, 1000),
+                'voc': random.randint(1, 1000)
+            }
+        ]
+    }
+    return jsonify(return_data)
+
+
+@app.route('/send-data', methods=['GET', 'POST'])
+def send_data():
+    try:
+        data = request.get_json(force=True)
+        print(data)
+        return "Done"
+    except Exception as e:
+        return e
