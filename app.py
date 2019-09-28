85c903f92d24df3d9fa7988079dde9a8ba905d97
rijun <richard.jung001@stud.fh-dortmund.de>
2019-09-29 01:01:14 +0200

    Tweaked values

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def get_data():
             'id': "STAT1",
             'name': "Station 1",
             'date_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'no2': random.randint(1, 1000),
-            'pm10': random.randint(1, 1000)
+            'no2': random.randint(15, 200),
+            'pm10': random.randint(20, 70)
         },
         'intern': [
             {
@@ -28,16 +28,16 @@ def get_data():
                 'name': "Raum 1",
                 'temp': random.randint(1, 1000),
                 'hum': random.randint(1, 1000),
-                'co2': random.randint(1, 1000),
-                'voc': random.randint(1, 1000)
+                'co2': random.randint(500, 3000),
+                'voc': random.randint(300, 1500)
             },
             {
                 'id': "R2",
                 'name': "Raum 2",
                 'temp': random.randint(1, 1000),
                 'hum': random.randint(1, 1000),
-                'co2': random.randint(1, 1000),
-                'voc': random.randint(1, 1000)
+                'co2': random.randint(500, 3000),
+                'voc': random.randint(300, 1500)
             }
         ]
     }
@@ -46,9 +46,11 @@ def get_data():
 
 @app.route('/send-data', methods=['GET', 'POST'])
 def send_data():
+    print("Connection")
     try:
         data = request.get_json(force=True)
         print(data)
         return "Done"
     except Exception as e:
+        print(e)
         return e
