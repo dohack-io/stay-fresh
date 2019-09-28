2f677afe031adfafa6054b4c2349e39ae5b8f518
rijun <richard.jung001@stud.fh-dortmund.de>
2019-09-29 01:35:29 +0200

    Finished Neonious simulation

--- server_neonious.py
+++ server_neonious.py
@@ -1,34 +1,35 @@
-    # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Sat Sep 28 10:58:41 2019
 
 @author: Benjamin Weber
 """
+
+
 # server_neonious.py
 
-def parsePacket(input_string):  
-
+def parsePacket(input_string):
+    print(input_string)
     if input_string == 'Hello':
         res = 0
     else:
         res = -1
     return res
 
-def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
 
+def client_thread(conn, ip, port, MAX_BUFFER_SIZE=4096):
     input_from_client_bytes = conn.recv(MAX_BUFFER_SIZE)
-
 
     import sys
     siz = sys.getsizeof(input_from_client_bytes)
-    if  siz >= MAX_BUFFER_SIZE:
+    if siz >= MAX_BUFFER_SIZE:
         print("Packet size is to large: {}".format(siz))
 
     # decode and parse packet from neonious
     input_from_client = input_from_client_bytes.decode("utf8").rstrip()
     print("Packet content: {}".format(input_from_client))
     res = parsePacket(input_from_client)
-    
+
     if res == 0:
         print("packet valid!")
     else:
@@ -37,22 +38,22 @@ def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
     conn.close()
     print('Connection ' + ip + ':' + port + " ended")
 
-def start_server():
 
+def start_server():
     import socket
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print('Socket created')
 
     try:
-        soc.bind(("192.168.0.100", 12345))
+        soc.bind(("192.168.0.42", 12345))
         print('Socket bind complete')
     except socket.error as msg:
         import sys
         print('Bind failed. Error : ' + str(sys.exc_info()))
         sys.exit()
 
-    #start listening
+    # start listening
     soc.listen(10)
     print('Socket now listening')
 
@@ -71,5 +72,3 @@ def start_server():
             import traceback
             traceback.print_exc()
     soc.close()
-
-start_server()  
--- server_neonious_2.js
+++ server_neonious_2.js
@@ -1,14 +1,17 @@
 const axios = require('axios');
 let gpio = require('gpio');
+let lowsys = require('lowsys')
 
 let ledRedState = false;
 
+let n_name = "Office 1";
+
 function send_data() {
 
-  let co2 = 400 + 2600*Math.random();//400 - 3000
-  let hum = 10 + 90*Math.random();
-  let voc = 400 + 600*Math.random();
-  let tmp = 5 + 25*Math.random();
+  let n_co2 = 400 + 2600*Math.random();//400 - 3000
+  let n_hum = 10 + 90*Math.random();
+  let n_voc = 400 + 600*Math.random();
+  let n_tmp = 5 + 25*Math.random();
 
 
   ledRedState = !ledRedState;
@@ -16,19 +19,20 @@ function send_data() {
 
   axios
     .post('http://192.168.0.42:5000/send-data', {
-      name: 'Neo1',
-      co2: co2,
-      hum: hum,
-      voc: voc,
-      tmp: tmp
+      name: n_name,
+      co2: n_co2,
+      hum: n_hum,
+      voc: n_voc,
+      tmp: n_tmp
     })
     .then(res => {
-      console.log(statusCode: ${res.statusCode})
+      console.log("statusCode: ${res.statusCode}")
       console.log(res)
     })
     .catch(error => {
       console.error(error)
     });
+    lowsys.kickWatchdog();
 }
 
-setInterval(send_data, 1000);
+setInterval(send_data, 5000);
