ffbed8fd7946fa80a760cb9ba5976345d0f6a53f
rijun <richard.jung001@stud.fh-dortmund.de>
2019-09-29 02:03:31 +0200

    Ready for merge

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
+room_1 = {
+    'id': "",
+    'name': "",
+    'co2': 0,
+    'hum': 0,
+    'voc': 0,
+    'tmp': 0
+}
+
+room_2 = {
+    'id': "",
+    'name': "",
+    'co2': 0,
+    'hum': 0,
+    'voc': 0,
+    'tmp': 0
+}
+
+room_list = [room_1, room_2]
+
 
 @app.route('/')
 def root():
@@ -22,35 +42,27 @@ def get_data():
             'no2': random.randint(15, 200),
             'pm10': random.randint(20, 70)
         },
-        'intern': [
-            {
-                'id': "R1",
-                'name': "Raum 1",
-                'temp': random.randint(1, 1000),
-                'hum': random.randint(1, 1000),
-                'co2': random.randint(500, 3000),
-                'voc': random.randint(300, 1500)
-            },
-            {
-                'id': "R2",
-                'name': "Raum 2",
-                'temp': random.randint(1, 1000),
-                'hum': random.randint(1, 1000),
-                'co2': random.randint(500, 3000),
-                'voc': random.randint(300, 1500)
-            }
-        ]
+        'intern': room_list
     }
     return jsonify(return_data)
 
 
 @app.route('/send-data', methods=['GET', 'POST'])
 def send_data():
-    print("Connection")
-    try:
-        data = request.get_json(force=True)
-        print(data)
-        return "Done"
-    except Exception as e:
-        print(e)
-        return e
+    data = request.get_json(force=True)
+    if data['id'] == "O1":
+        room_1['id'] = data['id']
+        room_1['name'] = data['name']
+        room_1['temp'] = int(data['tmp'])
+        room_1['hum'] = int(data['hum'])
+        room_1['co2'] = int(data['co2'])
+        room_1['voc'] = int(data['voc'])
+    elif data['id'] == "O2":
+        room_2['id'] = data['id']
+        room_2['name'] = data['name']
+        room_2['temp'] = int(data['tmp'])
+        room_2['hum'] = int(data['hum'])
+        room_2['co2'] = int(data['co2'])
+        room_2['voc'] = int(data['voc'])
+    print(data)
+    return "Done"
